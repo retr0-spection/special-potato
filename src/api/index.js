bda42ebcb435ff2adcd4f0701d5b5f6bae171f6a
retr0-spection <ratinailana@icloud.com>
2024-05-06 15:44:25 +0200

    save

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const domain = 'http://localhost:3001/api' // "https://espazaserver.azurewebsites.net/api" 
+const domain =  "https://espazaserver.azurewebsites.net/api" 
 //https://espazaserver.azurewebsites.net/auth/authenticate
 
 const API = {}
